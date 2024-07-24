9feb9bbce4e6b3462e831a939a4128576c896b10
jbraddez <157930657+jbraddez@users.noreply.github.com>
2024-07-24 15:21:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      Image.getElementById("header").style.height = "50px";
+    } else {
+      Image.getElementById("header").style.height = "110px";
+    }
+  }
+
+
+
+
+
